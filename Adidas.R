d200470411fa4cbf6712b7052f5e3a0d64ac15e7
Izabela <iza.szymkowiak0109@gmail.com>
2025-07-07 15:41:31 +0200

    Total Sales per Region

--- Adidas.R
+++ Adidas.R
@@ -11,6 +11,8 @@ library(RKaggle)
 library(Hmisc)
 library(dplyr)
 library(ggplot2)
+library(scales)
+
 dataset <- RKaggle::get_dataset("heemalichaudhari/adidas-sales-dataset")
 head(dataset)
 dataset = dataset[3:nrow(dataset), ]
@@ -20,12 +22,17 @@ colnames(dataset) = dataset[1, ]
 dataset = dataset[-1, ]
 head(dataset)
 
+dataset[, c("Retailer", "Region", "State", "City", "Product", "Sales Method")] <- lapply(dataset[, c("Retailer", "Region", "State", "City", "Product", "Sales Method")], as.character)
+dataset[, c("Retailer ID", "Price per Unit", "Units Sold", "Total Sales", "Operating Profit", "Operating Margin")] <- lapply(dataset[, c("Retailer ID", "Price per Unit", "Units Sold", "Total Sales", "Operating Profit", "Operating Margin")], as.numeric)
+dataset$`Invoice Date` <- as.Date(as.numeric(dataset$`Invoice Date`), origin = "1899-12-30")
+
 describe(dataset)
+
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Sales"),
+  titlePanel("Adidas Sales Analysis"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -34,15 +41,13 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Input: Slider for the number of bins ----
-      selectInput(inputId = "selectedRetailer", label = "select option", selected = "Walmart", choices = dataset$Retailer)
+      selectInput(inputId = "selectedRetailer", label = "Select Retailer", selected = "Walmart", choices = dataset$Retailer)
     ),
     # Main panel for displaying outputs ----
     mainPanel(
       
       # Output: Histogram ----
       plotOutput("chart")
-      #verbatimTextOutput("chart")
-      
     )
   )
 )
@@ -51,10 +56,15 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
-  
   output$chart <- renderPlot({
+    si_labels <- label_number(scale_cut = cut_si(unit = ""))
     subset <- dataset[dataset$Retailer == input$selectedRetailer, ]
-    p <- ggplot(data = subset) + geom_bar(mapping = aes(x = State))
+    
+    sales_per_region =  subset %>%
+      group_by(Region) %>%
+      summarise(totalSales = sum(`Total Sales`))
+    
+    p <- ggplot(sales_per_region, aes(x = Region, y = totalSales)) + geom_col(fill = "steelblue") + labs(title = paste("Total Sales by Region:", input$selectedRetailer), x = "Region", y = "Total Sales") + scale_y_continuous(labels = si_labels) + theme_minimal() + geom_text(aes(label = totalSales), vjust = -0.5, size = 3)
     p
   })
 
